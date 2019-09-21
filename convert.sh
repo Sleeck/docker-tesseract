6a5e67f0bdc2684e3501e213272bf35644f77753
Unknown <gitkraken@sleeck.eu>
2019-09-21 22:39:51 +0200

    Add chown

--- convert.sh
+++ convert.sh
@@ -13,3 +13,7 @@ then
     tesseract $1 ./$(basename $1 .jpg) -l $TESSERACT_LANGUAGE pdf
 fi
 
+if [ ! -z "$TESSERACT_OUTPUT_USER_GROUP" ]
+then
+    chown $TESSERACT_OUTPUT_USER_GROUP ./$(basename $1 .jpg).pdf
+fi
